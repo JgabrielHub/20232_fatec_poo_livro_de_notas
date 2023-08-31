1d174e1f447f695be4eaa679023b4781d42d0005
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-08-31 18:02:46 -0300

    ap06(java): teste do livro de notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,9 @@
+public class TesteLivroDeNotas{
+    public static void main(String args[]){
+        //variável de referência
+        //malloc
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+        System.out.println("Fim");
+    }
+}
