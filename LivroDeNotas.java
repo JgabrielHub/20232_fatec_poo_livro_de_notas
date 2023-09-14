3a2358ad6b15988041cf2240cd880a6fc63b4d04
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-09-14 16:07:19 -0300

    ap06(java): fim da listagem 12.1

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,5 +1,14 @@
 public class LivroDeNotas{
+    //variavel de instancia
+    //instancia pe sinonimo de objeto
+    String nomeDisciplina;
+
     public void exibirMensagem(){
-        System.out.println("Bem vindo ao livro de notas");
+        System.out.printf("Bem vindo ao livro de notas de %s\n", nomeDisciplina);
+    }
+    // construtor padrão (aquele cusa lista de parametros é vazia)
+    LivroDeNotas(String n){
+        //shadowing
+       this.nomeDisciplina = n;
     }
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,8 +1,12 @@
 public class TesteLivroDeNotas{
     public static void main(String args[]){
-        //variável de referência
-        //malloc
-        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        //nome completamente qualificado  classe.
+        String nomeDisciplina = javax.swing.JOptionPane.showInputDialog("digite o nome da disciplina");
+        //inserferencia de tipo
+        //var: somente java 10
+        var livroDeNotas = new LivroDeNotas(nomeDisciplina);
+        //livroDeNotas.nomeDisciplina = nomeDisciplina;
+        // enviando a men
         livroDeNotas.exibirMensagem();
         System.out.println("Fim");
     }
