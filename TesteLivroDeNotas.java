e3e0a808b7de56e8358d5f3f06ff24d268d5c009
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-09-14 16:49:49 -0300

    ap06(java): fim da apostila

--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -6,10 +6,12 @@ public class TesteLivroDeNotas{
         //var: somente java 10
         var livroDeNotas = new LivroDeNotas(nomeDisciplina);
         //livroDeNotas.nomeDisciplina = nomeDisciplina;
-        //enviando a mensagem
+        //enviando a mensagem "exibirMensagem" ao objeto livroDeNotas
         livroDeNotas.exibirMensagem();
         //livroDeNotas.nomeDisciplina = "Português";
         System.out.println("A disciplina é: " + livroDeNotas.getNomeDisciplina());
+        String nome = "ABC";
         System.out.println("Fim");
+        lilivroDeNotas.nomeDisciplina = "Português";
     }
 }
